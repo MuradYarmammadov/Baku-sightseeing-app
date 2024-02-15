7b1c6087a285664f5c37d1bcf16a641f936f7630
Murad Yarmammadov <yarmamedov.murad01@gmail.com>
2024-02-15 12:32:03 +0100

    Model and Data Service finished

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Murad Yarmamedov on 15.02.24.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Murad Yarmamedov on 15.02.24.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description : String
+    let imageNames: [String]
+    let link: String
+}
